e38250a8e6fffd047f255a17792520fcde6f7bf6
Chiaraplizz <chiara.plizzari@mail.polimi.it>
2020-09-15 23:21:22 +0200

    Update ntu_gen_bones.py

--- code/tools/ntu_gen_bones.py
+++ code/tools/ntu_gen_bones.py
@@ -42,10 +42,10 @@ def gen_bone_data():
     for dataset in datasets:
         for set in sets:
             print(dataset, set)
-            data = np.load('{}/{}_data_joint.npy'.format(dataset, set))
+            data = np.load('./{}/{}_data_joint.npy'.format(dataset, set))
             N, C, T, V, M = data.shape
             fp_sp = open_memmap(
-                '{}/{}_data_bone.npy'.format(dataset, set),
+                './{}/{}_data_bone.npy'.format(dataset, set),
                 dtype='float32',
                 mode='w+',
                 shape=(N, 3, T, V, M))
