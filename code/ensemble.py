0540f46a2df62084dbb3bd0b562a7e1c4007846c
Chiara <chiara.plizzari@mail.polimi.it>
2020-12-07 22:57:10 +0100

    Fixed emsemble

--- code/ensemble.py
+++ code/ensemble.py
@@ -25,15 +25,14 @@ r2 = open('./epoch1_test_score.pkl', 'rb')
 r2 = list(pickle.load(r2).items())
 right_num = total_num = right_num_5 = 0
 print(label.size)
-for i in tqdm(range(60)):
+for i in tqdm(range(len(label[0]))):
     _, l = label[:, i]
     _, r11 = r1[i]
-    _, r22 = r2[i][:60]
+    _, r22 = r2[i]
     r = r11 + r22 * arg.alpha
     rank_5 = r.argsort()[-5:]
     right_num_5 += int(int(l) in rank_5)
     r = np.argmax(r)
-    #print(r)
     right_num += int(r == int(l))
     total_num += 1
 acc = right_num / total_num
